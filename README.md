948996cce58e1470f15aed5d1fcf09ac1862ad08
mbatou <georgesmbatoucharles@gmail.com>
2023-10-02 17:59:46 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,10 @@
+# adsparo_launch1
+
+Content 
+- Adsparo chat with typical answers and variables
+- using Javascript to make answers and buttons dynamic
+
+Technology = html - css (Tailwind) and javascript 
+Inline css is included for better use and adapatability 
+
+V1 doesn't include Firebase cause facing some bugs. 
---
+++ main.js
@@ -0,0 +1,31 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        // const firebaseConfig = {
+        //     apiKey: "AIzaSyDlq4ji3ZEpmg66ZzGpxUfo1yi3YgraqFU",
+        //     authDomain: "adsparo-game.firebaseapp.com",
+        //     projectId: "adsparo-game",
+        //     storageBucket: "adsparo-game.appspot.com",
+        //     messagingSenderId: "920736398837",
+        //     appId: "1:920736398837:web:ffae08c670be89ff5574bc",
+        //     measurementId: "G-LDQW499Q8H"
+        // };
+    
+        // // Initialize Firebase
+        // firebase.initializeApp(firebaseConfig);
+    
+        // // Firebase Firestore
+        // const db = firebase.firestore();
+
+
+        // Function to save the email to Firestore
+        // function saveEmailToFirestore(email) {
+        //     db.collection("registrations")
+        //         .add({
+        //             email: email
+        //         })
+        //         .then(() => {
+        //             console.log("Email saved to Firestore: " + email);
+        //         })
+        //         .catch(error => {
+        //             console.error("Error saving email to Firestore: " + error);
+        //         });
+        // }
